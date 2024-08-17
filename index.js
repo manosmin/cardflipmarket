c134664637e874e13d2b44279145f8b9e3b394fa
manosmin <manosmin@hotmail.com>
2024-08-17 07:32:35 +0300

    added code for production

--- index.js
+++ index.js
@@ -23,3 +23,11 @@ app.listen(port, () => {
 
 connectDB().then(() => { runScheduledTask(); });
 
+if (process.env.NODE_ENV === "PRODUCTION") {
+    
+    app.use(express.static("client/build"));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
